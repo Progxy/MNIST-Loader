48fd3946df313852af7d794dd3ea4b7eeea256bd
TheProgxy <theproxy@gmail.com>
2024-12-28 15:36:02 +0100

    fix

--- mnist_loader.py
+++ mnist_loader.py
@@ -4,6 +4,7 @@ import random
 import struct
 import matplotlib.pyplot as plt
 import os
+import gzip
 
 class Transformations:
     def __init__(self, images): 
@@ -13,8 +14,10 @@ class Transformations:
     def apply_scaling(self, scales=[0.8, 1.0, 1.2]):
         for scale in scales:
             for img in self.images[:]:
+                height, width = img.shape[:2]
                 scaled = cv2.resize(img, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
-                self.images.append(scaled)
+                image = cv2.resize(scaled, (width, height))
+                self.images.append(image)
 
     def apply_translation(self, translations=[(-10, -10), (10, 10), (0, 20)]):
         for tx, ty in translations:
@@ -46,13 +49,13 @@ class Transformations:
             self.images.append(cv2.flip(img, 1))  # Horizontal flip
             self.images.append(cv2.flip(img, 0))  # Vertical flip
 
-    def apply_contrast_adjustment(self, contrast_factors=[0.8, 1.0, 1.2]):
+    def apply_contrast_adjustment(self, contrast_factors=[0.8, 1.2]):
         for alpha in contrast_factors:
             for img in self.images[:]:
                 adjusted = cv2.convertScaleAbs(img, alpha=alpha, beta=0)
                 self.images.append(adjusted)
 
-    def apply_gaussian_noise(self, noise_levels=[10, 20]):
+    def apply_gaussian_noise(self, noise_levels=[10, 20, 30]):
         for noise_level in noise_levels:
             for img in self.images[:]:
                 noise = np.random.normal(0, noise_level, img.shape).astype(np.uint8)
@@ -87,7 +90,7 @@ class Transformations:
         return self.images
 
 class DatasetGenerator:
-    def __init__(self, labels_file_prefix, images_file_prefix, images_paths_and_labels = {}, width = 28, height = 28, noise_cnt = 10):
+    def __init__(self, labels_file_prefix, images_file_prefix, images_paths_and_labels = {}, width = 28, height = 28, noise_cnt = 10, use_compression = False):
         self.labels_file_prefix = labels_file_prefix
         assert (self.labels_file_prefix != "")
         self.images_file_prefix = images_file_prefix
@@ -96,6 +99,7 @@ class DatasetGenerator:
         self.dataset = {}
         self.width = width
         self.height = height
+        self.use_compression = use_compression
         self.labels = list(images_paths_and_labels.keys())
 
         self.images = {}
@@ -193,6 +197,7 @@ class DatasetGenerator:
         assert (self.dataset != {})
 
         labels_file_name = f"{self.labels_file_prefix}-idx1-ubyte"
+        if self.use_compression: labels_file_name += ".gz"
         labels_magic = 2049
         
         size = 0
@@ -200,29 +205,45 @@ class DatasetGenerator:
             size += len(self.dataset[label])
 
         print(f"Generating file {labels_file_name} with {size} labels...")
-        with open(labels_file_name, "wb") as file:
-            file.write(struct.pack(">I", labels_magic))
-            file.write(struct.pack(">I", size))
-            for label in self.labels:
-                for _ in range(len(self.dataset[label])):
-                    file.write(struct.pack(">B", label))
+        labels_data = b""
+        labels_data += struct.pack(">I", labels_magic)
+        labels_data += struct.pack(">I", size)
+        for label in self.labels:
+            labels_data += struct.pack(">B", label) * len(self.dataset[label])
+        
+        print("Writing data to file...")
+        
+        if self.use_compression: 
+            with gzip.open(labels_file_name, 'wb', compresslevel=6) as f:
+                f.write(labels_data)
+        else:
+            with open(labels_file_name, "wb") as f:
+                f.write(labels_data)
 
         print("File generated successfully.")
         
         images_file_name = f"{self.images_file_prefix}-idx3-ubyte"
+        if self.use_compression: images_file_name += ".gz"
         images_magic = 2051
 
         print(f"Generating file {images_file_name} with {size} images...")
 
-        with open(images_file_name, "wb") as file:
-            file.write(struct.pack(">I", images_magic))
-            file.write(struct.pack(">I", size))
-            file.write(struct.pack(">I", self.height))
-            file.write(struct.pack(">I", self.width))
-            for label in self.labels:
-                for image in self.dataset[label]:
-                    image = cv2.resize(image, (self.width, self.height))
-                    file.write(image.tobytes())
+        images_data = b''
+        images_data += struct.pack(">I", images_magic)
+        images_data += struct.pack(">I", size)
+        images_data += struct.pack(">I", self.height)
+        images_data += struct.pack(">I", self.width)
+        for label in self.labels:
+            images_data += b''.join(image.tobytes() for image in self.dataset[label])
+
+        print("Writing data to file...")
+
+        if self.use_compression: 
+            with gzip.open(images_file_name, 'wb', compresslevel=6) as f:
+                f.write(images_data)
+        else:
+            with open(images_file_name, "wb") as f:
+                f.write(images_data)
 
         print("File generated successfully.")
 
@@ -231,21 +252,33 @@ class DatasetGenerator:
     def load_mnist_format_dataset(self):
         label_path = f"{self.labels_file_prefix}-idx1-ubyte"
         image_path = f"{self.images_file_prefix}-idx3-ubyte"
+        if self.use_compression: 
+            label_path += ".gz"
+            image_path += ".gz"
 
         labels = []
+        labels_data = b""
         with open(label_path, 'rb') as f:
-            magic, size = struct.unpack('>II', f.read(8))
-            assert (magic == 2049)
-            print(f"magic: {magic}, size: {size}")
-            labels = np.frombuffer(f.read(size), dtype=np.uint8).tolist()
+            labels_data = f.read()
+
+        if self.use_compression: labels_data = gzip.decompress(labels_data)
+        magic, size = struct.unpack('>II', labels_data[:8])
+        assert (magic == 2049)
+        print(f"magic: {magic}, size: {size}")
+        labels = np.frombuffer(labels_data[8:], dtype=np.uint8).tolist()
 
         images = []
+        images_data = b""
         with open(image_path, 'rb') as f:
-            magic, size, rows, cols = struct.unpack('>IIII', f.read(16))
-            assert (magic == 2051)
-            num_pixels = size * rows * cols
-            image_data = np.frombuffer(f.read(num_pixels), dtype=np.uint8)
-            images = image_data.reshape((size, rows, cols))
+            images_data = f.read()
+
+        if self.use_compression: images_data = gzip.decompress(images_data)
+        magic, size, rows, cols = struct.unpack('>IIII', images_data[:16])
+        print(f"magic: {magic}, size: {size}, rows: {rows}, cols: {cols}")
+        assert (magic == 2051)
+        num_pixels = size * rows * cols
+        image_data = np.frombuffer(images_data[16:16+num_pixels], dtype=np.uint8)
+        images = image_data.reshape((size, rows, cols))
             
         return images, labels
 
@@ -289,7 +322,7 @@ if __name__ == "__main__":
     paths_and_labels = find_dataset_images("./example", "bpen")
 
     # First generate the dataset:              -- labels_prefix --                   -- images prefix --                             -- width & height --
-    dataset_generator = DatasetGenerator("./dataset/my-dataset-train-labels", "./dataset/my-dataset-train-images", paths_and_labels,        28, 28)
+    dataset_generator = DatasetGenerator("./dataset/my-dataset-train-labels", "./dataset/my-dataset-train-images", paths_and_labels,        28, 28, use_compression=True)
     # Generate multiple transformations of the given images, effectively populating the dataset (which at this point is a dictionary)
     dataset_generator.generate_dataset()
     # Save the dataset using the mnist format
@@ -297,12 +330,12 @@ if __name__ == "__main__":
 
 
     # Do the same for the test dataset
-    dataset_generator = DatasetGenerator("./dataset/my-dataset-test-labels", "./dataset/my-dataset-test-images", {0: ["./example/g_bpen.png"], 1: ["./example/y_bpen.png"], 2: ["./example/b_bpen.png"], 3: ["./example/t_bpen.png"]}, 28, 28, 2)
+    dataset_generator = DatasetGenerator("./dataset/my-dataset-test-labels", "./dataset/my-dataset-test-images", {0: ["./example/g_bpen.png"], 1: ["./example/y_bpen.png"], 2: ["./example/b_bpen.png"], 3: ["./example/t_bpen.png"]}, 28, 28, 2,use_compression=True)
     dataset_generator.generate_dataset()
     dataset_generator.store_dataset_as_mnist_format()
 
     # To load the dataset specify the prefixes as above
-    dataset_viewer = DatasetGenerator("./dataset/my-dataset-train-labels", "./dataset/my-dataset-train-images")
+    dataset_viewer = DatasetGenerator("./dataset/my-dataset-train-labels", "./dataset/my-dataset-train-images",use_compression=True)
     # load the labels and the images
     x_train, y_train = dataset_generator.load_mnist_format_dataset()
     # Plot every image inside x_train
